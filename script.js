5750508ac7f90208f42fa123ea6994b2dd13266d
Andreza Melo <meloandrezas65@gmail.com>
2026-06-09 15:22:11 -0300

    Initialize Finanças AnJo and display current month

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Finanças AnJo iniciado 🚀");
+
+const meses = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro"
+];
+
+const hoje = new Date();
+
+document.getElementById("mesAtual").textContent =
+  `${meses[hoje.getMonth()]} ${hoje.getFullYear()}`;
