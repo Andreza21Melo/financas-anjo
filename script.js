0917babfb789b4bfdcd68a3cdd3a607a33f0f689
Andreza Melo <meloandrezas65@gmail.com>
2026-06-16 10:21:03 -0300

    Remove obterPeriodoMes function
    Removed the obterPeriodoMes function that calculates the start of the month and the next month.

--- script.js
+++ script.js
@@ -132,32 +132,6 @@ btnMesAnterior.addEventListener(
 
     atualizarMesTela();
 
-    function obterPeriodoMes(){
-
-  const inicioMes =
-    `${anoSelecionado}-${String(
-      mesSelecionado + 1
-    ).padStart(2, "0")}-01`;
-
-  let proximoMes = mesSelecionado + 2;
-  let proximoAno = anoSelecionado;
-
-  if (proximoMes > 12) {
-    proximoMes = 1;
-    proximoAno++;
-  }
-
-  const inicioProximoMes =
-    `${proximoAno}-${String(
-      proximoMes
-    ).padStart(2, "0")}-01`;
-
-  return {
-    inicioMes,
-    inicioProximoMes
-  };
-  }
-
     await carregarHistorico();
     await carregarSaldo();
 
